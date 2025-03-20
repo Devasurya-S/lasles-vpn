668a9dbd2f7a532f2e4f205a63f7159cec59b76b
Devasurya-S <devasuryasuresh@gmail.com>
2025-03-20 14:20:30 +0530

    price cards finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
-import StatCard from './components/statCard';
+import StatCard from './components/StatCard';
 import Feature from './components/Feature';
 import PriceSection from './components/PriceSection';
 import MapSection from './components/MapSection';
